1386152b3090d80c62776af5fa8bf54d8c972103
Diana Suvorova <diana.suvorova@gmail.com>
2015-08-27 16:30:57 -0700

    the beginning of studentDetails

--- modules/student/studentDetails.react.js
+++ modules/student/studentDetails.react.js
@@ -5,74 +5,21 @@ var assign = require('object-assign');
 
 var Calendar = require('../calendar/calendar.react');
 
-var StudentStore = require('../../stores/studentStore');
-
 var studentEdit = module.exports = React.createClass({
-
-  getInitialState: function () {
-    return (this.props.student) ? this.setState(this._getState(this.props.student.groupId)) : null;
-  },
-
-  componentWillReceiveProps: function(nextProps) {
-    var groupId = (nextProps.student) ? nextProps.student.groupId : null;
-    this.setState(this._getState(groupId, nextProps.dateRange));
-  },
 
   render: function () {
 
-    var classes = {
-      studentEdit : ClassNames({
-        'active': this.props.student
-      })
-    };
-
-    var selectGroups =
-        <select onChange = {this._onSelectGroup} defaultValue = {this.state.groupId}>
-            {
-              Object.keys(this.props.groups).map(function(groupId) {
-                return <option key = {groupId} value = {groupId}>{this.props.groups[groupId].name}</option>;
-              }.bind(this))
-            }
-          </select>;
-
-      var currentSchedule = (this.props.student.name) ?
-      <div>
-        <span>{'Current schedule in ' + this.props.groups[this.props.student.groupId].name}</span>
-        <Calendar schedule = {this.props.student.schedule} edit = {false} group = {false} />
-      </div> : null;
-
-
     return (
-      <div id = 'studentEdit' className = {classes.studentEdit}>
-        <div className = 'header'> {this.props.student.name || 'New Student'}</div>
+      <div id = 'studentDetails'>
+      <span className = 'actionItem warning close' onClick = {this._onCloseEditStudent}><i className = 'fa fa-times'></i></span>
         <div className = 'details'>
           <div>
             <input className = 'name' defaultValue = {this.props.student.name} placeholder = {'name'}></input>
             <input className = 'birthdate' defaultValue = {this._formatDate(this.props.student.birthdate)} placeholder = {'birthdate'}></input>
           </div>
-          {currentSchedule}
-          <div>
-            <span>{'New schedule for the student'}</span>
-            <Calendar schedule = {this.props.student.schedule} edit = {true}  group = {false}  updateSchedule = {this._updateSchedule}/>
-          </div>
-          <div>
-            <span>{'Available spots in the group '}</span>
-            {selectGroups}
-            <Calendar schedule = {this.state.availableSchedule} edit = {false} group = {true}/>
-          </div>
-          <div>
-            <span>{'Next availaility in this group for the new schedule is: '}</span>
-            <span>{'Nov ' +  Math.floor(Math.random() * 30 + 1)} </span>
-          </div>
-
         </div>
       </div>
     );
-
-  },
-
-  _onSelectGroup: function () {
-    this.setState(this._getState(event.target.value));
   },
 
   _formatDate: function (date) {
@@ -80,21 +27,8 @@ var studentEdit = module.exports = React.createClass({
     if (date) return format2Digit(date.getMonth() + 1) + '/' + format2Digit(date.getDate()) + '/' + date.getFullYear();
   },
 
-  _updateSchedule: function (schedule) {
-      console.log('recalculate state for student edit');
-
-  },
-
-  _getState: function (groupId, dateRange) {
-
-    var groupId = groupId || this.state && this.state.groupId;
-    var dateRange = dateRange || this.props && this.props.dateRange;
-    var groupSummary = (groupId) ? StudentStore.getGroupSummaryForGroupIdAndDateRange(groupId, dateRange) : {};
-
-    return {
-      groupId: groupId,
-      availableSchedule: groupSummary.schedule
-    }
+  _onCloseEditStudent: function () {
+    this.props.closeStudentDetails();
   }
 
 });
--- modules/students/students.react.js
+++ modules/students/students.react.js
@@ -14,8 +14,6 @@ var DateRangeStore = require('../../stores/dateRangeStore');
 
 var students = module.exports = React.createClass({
 
-//  contextTypes: { router: React.PropTypes.func },
-
   getInitialState: function () {
     if (StudentStore.isEmpty()) {
       StudentAction.loadStudents(this.props.schoolId);
@@ -41,12 +39,12 @@ var students = module.exports = React.createClass({
   },
 
   render: function () {
-    var studentDetails = (this.state.activeStudent) ? <StudentDetails student = {this.state.activeStudent} groups = {this.state.groups} dateRangeObject = {this.state.dateRangeObject}/> : null;
+    var studentDetails = (this.state.activeStudent) ? <StudentDetails student = {this.state.activeStudent} groups = {this.state.groups} dateRangeObject = {this.state.dateRangeObject} closeStudentDetails = {this._closeStudentDetails}/> : null;
 
     var students = (StudentStore.isEmpty()) ?
       null :
       (<div id = 'students'>
-        <StudentList groupId = {this.state.groupId} dateRangeObject = {this.state.dateRangeObject} openStudent = {this._openStudent} activeStudent = {this.state.activeStudent} groups = {this.state.groups}/>
+        <StudentList groupId = {this.state.groupId} dateRangeObject = {this.state.dateRangeObject} openStudent = {this._openStudentDetails} activeStudent = {this.state.activeStudent} groups = {this.state.groups}/>
         {studentDetails}
       </div>)
 
@@ -57,12 +55,15 @@ var students = module.exports = React.createClass({
     this.setState(this._getState());
   },
 
-  _openStudent : function (student) {
+  _openStudentDetails : function (student) {
     this.setState(this._getState(student));
   },
 
+  _closeStudentDetails : function () {
+    this.setState({activeStudent: null});
+  },
+
   _getState: function (activeStudent) {
-//    var dateRangeKey = this.context.router.getCurrentParams().dateRange;
 
     var groups = StudentStore.getGroupsMap();
     var dateRangeObject = this.props.dateRangeObject || DateRangeStore.getCurrentDateRangeObject();
