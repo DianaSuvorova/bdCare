12a74204cb42f64f8626826ce22aac036c3a2a16
Boris Suvorov <bsuvorov@gmail.com>
2015-09-12 15:12:44 -0700

    Add ability to save schedule data for the next few month into  excel spreadsheet

--- stores/studentStore.js
+++ stores/studentStore.js
@@ -5,6 +5,10 @@ var Constants = require('../dispatcher/constants');
 var EventEmitter = require('events').EventEmitter;
 
 var OffLineData = require('./offlineData');
+var FileSaver = require('filesaver.js');
+
+var DateRangeStore = require('./dateRangeStore.js');
+
 
 var CHANGE_EVENT = 'change';
 var isOffline = false;
@@ -35,6 +39,7 @@ function setData(students, groups, mappings) {
     return mapping.attributes;
   });
 
+  saveToExcel();
   _isEmpty = false;
 }
 
@@ -50,12 +55,11 @@ function addStudent(student) {
   _students[student.id] = assign({}, {id: student.id}, student.attributes);
 }
 
-
 function getMappingsByGroupdIdAndDate(groupId, date) {
   return _mappings.filter(function (mapping) {
     return (mapping.groupId === groupId)
-    && (date > mapping.start_date)
-    && (!mapping.end_date || date < mapping.end_date)
+    && (date >= mapping.start_date)
+    && (!mapping.end_date || date <= mapping.end_date)
   });
 }
 
@@ -69,8 +73,8 @@ function getMappingsByStudentId(studentId) {
 function getMappingsByStudentIdAndDate(studentId, date) {
   return _mappings.filter(function (mapping) {
     return (mapping.studentId === studentId)
-    && (date > mapping.start_date)
-    && (!mapping.end_date || date < mapping.end_date)
+    && (date >= mapping.start_date)
+    && (!mapping.end_date || date <= mapping.end_date)
   });
 }
 
@@ -130,6 +134,8 @@ function minimumSlotsLoadForGroupIdForDateRange(groupId, dateRange) {
   return slotsBreakdown;
 }
 
+// returns a dictionary of date to dictionary of total count in am and pm range for this date
+// i.e. {"12-01-2015": {'am' : 10, 'pm':9}}
 function studentLoadForGroupIdForDateRange(groupId, dateRange) {
     var studentsCountPerDay = {};
 
@@ -178,6 +184,30 @@ function getStudentsIdsEligibleForUpgrade(groupId, dateRange) {
   return studentIds;
 }
 
+var StudentWeeklyScheduleRow = function () {
+  this.initialize.apply(this, arguments);
+  return this;
+};
+
+StudentWeeklyScheduleRow.prototype = {};
+
+StudentWeeklyScheduleRow.prototype.initialize = function (student, mapping) {
+  this.student = student;
+  this.mapping = mapping;
+}
+
+StudentWeeklyScheduleRow.prototype.serializeToList = function () {
+  var result = _slotsDict.map(function (dayTime){
+    if (this.mapping[dayTime] > 0) {
+      return (dayTime.indexOf("am") > -1) ? "8:00-12:30" : "12:30 - 5:00";
+    } else {
+      return "";
+    }
+  }.bind(this));
+  result.unshift(this.student.name)
+  return result;
+}
+
 var studentStore = module.exports = assign({}, EventEmitter.prototype, {
 
   emitChange: function () {
@@ -196,7 +226,7 @@ var studentStore = module.exports = assign({}, EventEmitter.prototype, {
     return _isEmpty;
   },
 
-  getStudentsMapForGroupIdAndDateRange: function (groupId, dateRange) {
+  getStudentsScheduleRowsForGroupIdDataRange: function(groupId, dateRange) {
     var startDate = dateRange[0];
     var endDate = dateRange[1];
     if (startDate > endDate) {
@@ -204,8 +234,15 @@ var studentStore = module.exports = assign({}, EventEmitter.prototype, {
       return null;
     }
 
-    var studentsMap = {};
+    var studentsList = getMappingsByGroupdIdAndDate(groupId, startDate).map(function (mapping) {
+      return new StudentWeeklyScheduleRow(_students[mapping.studentId], mapping);
+    });
+    return studentsList;
+  },
+
+  getStudentsMapForGroupIdAndDateRange: function (groupId, dateRange) {
     var listOfDates = createListOfDatesForDateRange(dateRange);
+    var studentsMap = [];
 
     listOfDates.forEach( function(date) {
       var groupMappings = getMappingsByGroupdIdAndDate(groupId, date);
@@ -364,3 +401,115 @@ studentStore.dispatchToken = Dispatcher.register( function (action) {
 
   }
 });
+
+function generateExcelWorksheetForDateRange(dateRange) {
+  var groupHeader = _slotsDict.slice();
+  groupHeader.unshift("Student name")
+
+  var data = []
+  Object.keys(_groups).map(function(groupId) {
+    var groupHeader = _slotsDict.slice();
+    groupHeader.unshift(_groups[groupId].name)
+    data.push(groupHeader);
+
+    var kidsCountPerDay = _slotsDict.map( function(grouId) {return 0})
+    var studentWeeklyScheduleRows = studentStore.getStudentsScheduleRowsForGroupIdDataRange(groupId, dateRange);
+    studentWeeklyScheduleRows.forEach(function (studentWeeklyScheduleRow){
+      var schedulePerDaySlot = studentWeeklyScheduleRow.serializeToList()
+      for (var i = 1; i < schedulePerDaySlot.length; i++) {
+          kidsCountPerDay[i-1] += (schedulePerDaySlot[i] === "") ? 0 : 1;
+      }
+      data.push(schedulePerDaySlot);
+    });
+    // insert empty cell to adjust for name for the total count of kids attending particular day time section
+    kidsCountPerDay.unshift("")
+    data.push(kidsCountPerDay)
+
+    data.push([""]);
+  });
+  return sheet_from_array_of_arrays(data);
+}
+
+function saveToExcel() {
+  var wb = new Workbook();
+
+  dateRangeMap = DateRangeStore.getDateRangeMap();
+  Object.keys(dateRangeMap).forEach(function (dateRangeName) {
+    var dateRange = dateRangeMap[dateRangeName].dateRange;
+    var ws = generateExcelWorksheetForDateRange(dateRange);
+    wb.SheetNames.push(dateRangeName);
+    wb.Sheets[dateRangeName] = ws;
+  })
+
+  var wbout = XLSX.write(wb, {bookType:'xlsx', bookSST:true, type: 'binary'});
+  FileSaver.saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), "test.xlsx")
+}
+
+function datenum(v, date1904) {
+    if(date1904) v+=1462;
+    var epoch = Date.parse(v);
+    return (epoch - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
+}
+
+function sheet_from_groups_student_row_schedules(groups_to_schedulerows) {
+  var ws = {}
+  var range = {s: {c:10000000, r:10000000}, e: {c:0, r:0 }};
+
+  var row = 0;
+  for (group in groups_to_schedulerows) {
+    scheduleRowsList = groups_to_schedulerows[group];
+    for (var col = 0; C != scheduleRowsList.length; ++col) {
+      var cell_ref = XLSX.utils.encode_cell({c:col,r:row});
+      ws[cell_ref] = {v: scheduleRowsList[col]};
+    }
+
+    row += 1;
+  }
+}
+
+function sheet_from_array_of_arrays(data, opts) {
+    var ws = {};
+    var range = {s: {c:10000000, r:10000000}, e: {c:0, r:0 }};
+    for(var R = 0; R != data.length; ++R) {
+        for(var C = 0; C != data[R].length; ++C) {
+            if(range.s.r > R) range.s.r = R;
+            if(range.s.c > C) range.s.c = C;
+            if(range.e.r < R) range.e.r = R;
+            if(range.e.c < C) range.e.c = C;
+
+            var cell = {v: data[R][C]};
+            if(cell.v == null) continue;
+            var cell_ref = XLSX.utils.encode_cell({c:C,r:R});
+
+            if (typeof cell.v === 'number') {
+              cell.t = 'n';
+            } else if(typeof cell.v === 'boolean') {
+              cell.t = 'b';
+            } else if(cell.v instanceof Date) {
+                cell.t = 'n'; cell.z = XLSX.SSF._table[14];
+                cell.v = datenum(cell.v);
+            } else {
+              cell.t = 's';
+            }
+
+            ws[cell_ref] = cell;
+        }
+    }
+    if(range.s.c < 10000000) ws['!ref'] = XLSX.utils.encode_range(range);
+    return ws;
+}
+
+
+function s2ab(s) {
+  var buf = new ArrayBuffer(s.length);
+  var view = new Uint8Array(buf);
+  for (var i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+  return buf;
+}
+
+
+function Workbook() {
+  if(!(this instanceof Workbook)) return new Workbook();
+  this.SheetNames = [];
+  this.Sheets = {};
+}
