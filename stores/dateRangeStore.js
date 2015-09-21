065fd3d09edbe1488bcaf5744b69a2ae6b3ac266
Diana Suvorova <diana.suvorova@gmail.com>
2015-09-21 12:01:04 -0700

    fixing getting month name in safari

--- stores/dateRangeStore.js
+++ stores/dateRangeStore.js
@@ -2,15 +2,18 @@ var assign = require('object-assign');
 var EventEmitter = require('events').EventEmitter;
 
 var _dateRangeMap = {};
+var _monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
 
 function setDateRangeMap () {
   var currentDate = new Date(), currentYear = currentDate.getFullYear(), currentMonth = currentDate.getMonth();
-  var locale = "en-us";
   for (var month = currentMonth; month < currentMonth + 3; month++) {
     var startDate = new Date(currentYear, month, 1);
-    var monthWord = startDate.toLocaleString(locale, { month: "long" });
+    var monthWord = _monthNames[startDate.getMonth()];
     _dateRangeMap[monthWord + ' ' + startDate.getFullYear()] = {
-      key: monthWord + ' ' + startDate.getFullYear(),
+      key: monthWord + ' ' + startDate.getFullYear().toString(),
       dateRange: [startDate, new Date(currentYear, month + 1, 1)]
     };
   }
