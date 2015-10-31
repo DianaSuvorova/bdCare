aefb0f1942d48d8a99e4dc1a544b917b8afa0151
Diana Suvorova <diana.suvorova@gmail.com>
2015-10-30 20:09:10 -0700

    scrolling

--- modules/group/group.react.js
+++ modules/group/group.react.js
@@ -35,7 +35,7 @@ var group = module.exports = React.createClass({
     return (
       <div className = 'group'>
         <div className = 'header'  onClick = {this._onClick}>{this.props.group.name}</div>
-        <div onClick = {this._onClick}>
+        <div className = 'groupInfo' onClick = {this._onClick}>
           <div className = 'subheader'>
             <span className= 'label'> {'TOTAL CAPACITY'} </span>
             <span className= 'value'>{this.props.group.capacity} </span>
