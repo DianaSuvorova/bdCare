c2f92b4c4dcaef5c945257f03cf88d7826006ff2
Diana Suvorova <diana.suvorova@gmail.com>
2015-11-10 08:15:52 -0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Day care manager.
+[Day care manager.](http://bdcare.parseapp.com/gh)
+
+parse + react + sass
+
+Known issues:
+* routing is not working correctly because of [parse/express issue](https://groups.google.com/forum/#!topic/parse-developers/qtMO41U5VOM)
--- modules/about/aboutus.react.js
+++ modules/about/aboutus.react.js
@@ -7,10 +7,10 @@ var aboutus = module.exports = React.createClass({
       {'We are the team and the family of 3. '}
       <br></br>
       {'We learned about waitlist and scheduling processes while looking for a day care for our son. '}
-      {'So we had an idea of a product that could help with scheduling and planning day care managers go through. '}
+      {'So we had an idea of a product that could help day care managers with scheduling and planning. '}
       {'Here you see a demo of such a product. '}
       <br></br>
-      {'We are looking for feedback and partnership from day care centers.'}
+      {'We are looking for feedback and partnership with day care centers.'}
       <br></br>
       {'Please contact us at diana.suvorova@gmail.com'}
       <p>
